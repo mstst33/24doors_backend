6af749c486c9efbe62aec6ec95cb7e94c0d0c075
ethenalee <yeonji900111@gmail.com>
2019-09-07 18:42:10 -0400

    google auth login added

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../auth');
 
-// backend API about user
-
-/*
-* Description: Register user
-* Function: Write user data into DB
-* TransferType: POST
-* Input Value:
-  ...
-* Return Value:
-  ...
-*/
-router.post('/register_user', function(req, res, next) {
-  res.send('success to register user');
+// GET login page
+router.get('/login', function(request, response) {
+  response.render('login', { user: request.user });
 });
+
+// GET route for when you click on login - passport authenticates through google
+router.get('/auth/google',
+  auth.passport.authenticate('google', { scope: ['openid email profile'] }));
+
+// If successful auth - redirects to home page, if not - redirects to /login
+router.get('/auth/google/callback',
+  auth.passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function(request, response) {
+    // Authenticated successfully
+    response.redirect('/');
+  });
+
+// GET logout route - will sign person out of session
+router.get('/logout', function(request, response) {
+  request.logout();
+  response.redirect('/');
+});
+
+// Route middleware to ensure user is authenticated.
+function ensureAuthenticated(request, response, next) {
+  if (request.isAuthenticated()) {
+    return next();
+  }
+  response.redirect('/login');
+}
 
 module.exports = router;
